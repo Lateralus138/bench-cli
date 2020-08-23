e35d657a58c7a2040170c8353fa28eaf581116c4
Lateraus138 <faithnomoread@yahoo.com>
2020-08-22 21:55:17 -0500

    Adding new feature.

--- src/windows/main.f95
+++ src/windows/main.f95
@@ -2,12 +2,15 @@ program main
     use main_mod, only: to_upper,get_operating_system
     implicit none
     integer :: argc,argi,comstat,tickinit,tickend
-    character(len=:), allocatable :: argv,args,argstr,quietcom,os
+    character(len=:), allocatable :: argv,args,argstr,quietcom,os,pre,post
+    os = get_operating_system()
     argc = command_argument_count()  
     if (argc .eq. 0) call exit(1)
     allocate(character(len=4096) :: argv)
     args = ''
     quietcom = ''
+    pre = ''
+    post = ''
     do argi=1, argc
         call get_command_argument(argi,argv)
         if (to_upper(adjustl(trim(argv))) .eq. '-H' .or. &
@@ -18,6 +21,9 @@ program main
                             '', &
                             ' OPTIONS:', &
                             '     -h, --help      This help message.', &
+                            '     -a, --alternate Use alternate shell.', &
+                            '                     Bash in Linux.', &
+                            '                     PowerShell in Windows.', &
                             '     --nonverbose    Run command silently.', &
                             '                     While running non-verbosely', &
                             '                     it may be better to wrap', &
@@ -30,12 +36,25 @@ program main
                             ''
             call exit()
         end if
-        if (to_upper(adjustl(trim(argv))) .eq. '--NONVERBOSE') then
-            os = get_operating_system()
-            if (os .eq. 'linux') then
-                quietcom = ' >/dev/null 2>&1 >/dev/null'
-            else
-                quietcom = ' > nul'
+        if (to_upper(adjustl(trim(argv))) .eq. '--NONVERBOSE' .or. &
+            to_upper(adjustl(trim(argv))) .eq. '-A' .or. &
+            to_upper(adjustl(trim(argv))) .eq. '--ALTERNATE') then
+            if (to_upper(adjustl(trim(argv))) .eq. '--NONVERBOSE') then
+                if (os .eq. 'linux') then
+                    quietcom = ' >/dev/null 2>&1 >/dev/null'
+                else
+                    quietcom = ' > nul'
+                end if
+            end if
+            if (to_upper(adjustl(trim(argv))) .eq. '-A' .or. &
+                to_upper(adjustl(trim(argv))) .eq. '--ALTERNATE') then
+                if (os .eq. 'linux') then
+                    pre = 'bash -c '//"'"
+                    post = "'"
+                else
+                    pre = 'Powershell -Command "'
+                    post = '"'
+                end if
             end if
         else
             args = args//' '//trim(argv)
@@ -44,7 +63,7 @@ program main
     end do
     deallocate(argv)
     call system_clock(count=tickinit)
-    call execute_command_line(argstr//quietcom,exitstat=comstat)
+    call execute_command_line(pre//argstr//quietcom//post,exitstat=comstat)
     if (comstat .gt. 0) call exit(2)
     call system_clock(count=tickend)
     print '(i0,a)', tickend - tickinit, 'ms'
